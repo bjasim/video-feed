001a38f1ea7206c5e91f5f08723920ae0044cf4a
Bakr <bakr22hj@gmail.com>
2023-12-03 19:00:32 -0500

    created the required functions for the rectangle and teh ellipse tools, just need to fix a few bugs in the to do list

--- sketch.js
+++ sketch.js
@@ -6,6 +6,13 @@ let stampSelect;
 let currentStamp;
 let items = [];
 let stampPreview;
+//------------------
+let toolSelect;
+let currentTool;
+let currentShape;
+let colorSelect;
+let thicknessSelect;
+
 
 function preload() {
   stamps[0] = createImg("assets/original.png").hide();
@@ -37,9 +44,14 @@ function setup() {
   stampSelect.option('Stamp 4');
   stampSelect.changed(changeStamp);
 
-  // stampPreview = createImg('');
-  // stampPreview.size(150, 150); // Set the size of the stamp preview
-  // stampPreview.position(20, 20); // Position the stamp preview
+  toolSelect = createSelect();
+  toolSelect.option('No Tool');
+  toolSelect.option('Rectangle');
+  toolSelect.option('Ellipse');
+  toolSelect.changed(changeTool);
+
+  colorSelect = createColorPicker('#000000');
+  thicknessSelect = createSlider(0, 10, 1);
 
 }
 
@@ -65,9 +77,15 @@ function draw() {
   for (let item of items) {
     if (item.type === 'stamp') {
       image(item.image, item.x - item.image.width / 2, item.y - item.image.height / 2);
+    } else if (item.type === 'Rectangle' || item.type === 'Ellipse') {
+      drawShape(item);
     }
-    // Add code here to handle other types of items (e.g., shapes)
   }
+
+  if (currentShape) {
+    drawShape(currentShape);
+  }
+
 }
 
 function changeFilter() {
@@ -92,13 +110,71 @@ function changeStamp() {
   }
 }
 
+// function mousePressed() {
+//   if (currentStamp && mouseX >= (windowWidth - 640) / 2 && mouseX <= (windowWidth + 640) / 2 && mouseY >= (windowHeight - 480) / 2 && mouseY <= (windowHeight + 480) / 2) {
+//     items.push({
+//       type: 'stamp',
+//       image: currentStamp,
+//       x: mouseX,
+//       y: mouseY
+//     });
+//   }
+// }
+
 function mousePressed() {
-  if (currentStamp && mouseX >= (windowWidth - 640) / 2 && mouseX <= (windowWidth + 640) / 2 && mouseY >= (windowHeight - 480) / 2 && mouseY <= (windowHeight + 480) / 2) {
-    items.push({
-      type: 'stamp',
-      image: currentStamp,
-      x: mouseX,
-      y: mouseY
-    });
+  if (mouseX >= (windowWidth - 640) / 2 && mouseX <= (windowWidth + 640) / 2 && mouseY >= (windowHeight - 480) / 2 && mouseY <= (windowHeight + 480) / 2) {
+    if (currentStamp) {
+      items.push({
+        type: 'stamp',
+        image: currentStamp,
+        x: mouseX,
+        y: mouseY
+      });
+    } else if (currentTool) {
+      currentShape = {
+        type: currentTool,
+        x1: mouseX,
+        y1: mouseY,
+        x2: mouseX,
+        y2: mouseY,
+        color: colorSelect.color(),
+        thickness: thicknessSelect.value()
+      };
+    }
   }
 }
+
+function mouseDragged() {
+  if (currentShape) {
+    currentShape.x2 = mouseX;
+    currentShape.y2 = mouseY;
+  }
+}
+
+function mouseReleased() {
+  if (currentShape) {
+    items.push(currentShape);
+    currentShape = null;
+  }
+}
+
+function changeTool() {
+  currentTool = toolSelect.value() === 'No Tool' ? null : toolSelect.value();
+}
+
+function drawShape(shape) {
+  stroke(shape.color);
+  strokeWeight(shape.thickness);
+  if (shape.type === 'Rectangle') {
+    rect(shape.x1, shape.y1, shape.x2 - shape.x1, shape.y2 - shape.y1);
+  } else if (shape.type === 'Ellipse') {
+    ellipse(shape.x1, shape.y1, shape.x2 - shape.x1, shape.y2 - shape.y1);
+  }
+}
+
+
+//To-do 
+//-The ellipse is drawing from the center, not from the corner, change it to draw from the corner
+//-I think this means the fill colour should be for the shape, not the border, what do you think: The user should be able to select the border and fill colour of the shapes being drawn
+//-Border thikness should have 4 options only, not slidebar
+//-When the stamp is selected and hovers over the shape, the stamp is under the shape layer, but when drawing it, it gets drawn over the shape, fix it. 
